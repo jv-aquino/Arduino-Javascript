3f031bc19ccf0cf78eab928f1ae04869eaa1a0c7
jv-aquino <jvaquinocontact@gmail.com>
2022-10-03 13:58:17 -0300

    + arduino programming

---
+++ arduino/arduinoProgram.ino
@@ -0,0 +1,46 @@
+int greenPin = 2;
+int yellowPin = 4;
+int redPin = 6;
+ 
+void setup() 
+{ 
+  pinMode(greenPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    
+    String receivedString = "";
+    
+    while (Serial.available() > 0) {
+      receivedString += char(Serial.read());
+    }
+    
+    Serial.println(receivedString);
+    
+    if(receivedString == "0") {
+      digitalWrite(greenPin, LOW);
+      digitalWrite(yellowPin, LOW);
+      digitalWrite(redPin, LOW);
+    }
+    else if (receivedString == "1") {
+      digitalWrite(greenPin, HIGH);
+      digitalWrite(yellowPin, LOW);
+      digitalWrite(redPin, LOW);
+    }
+    else if (receivedString == "2") {
+      digitalWrite(yellowPin, HIGH);
+      digitalWrite(greenPin, LOW);
+      digitalWrite(redPin, LOW);
+    }
+    else if (receivedString == "3") {
+      digitalWrite(redPin, HIGH);
+      digitalWrite(greenPin, LOW);
+      digitalWrite(yellowPin, LOW);
+    }
+  }
+}
